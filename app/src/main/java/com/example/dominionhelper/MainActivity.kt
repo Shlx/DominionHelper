59b083fd92e8647af49b81405705b55d07f449b3
Marvin <marvin-suhr@hotmail.de>
2025-04-10 21:59:51 +0200

    Basic card data class, list of cards in UI

--- app/src/main/java/com/example/dominionhelper/MainActivity.kt
+++ app/src/main/java/com/example/dominionhelper/MainActivity.kt
@@ -4,44 +4,121 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.dominionhelper.ui.theme.DominionHelperTheme
+import androidx.compose.material3.Card
+
+fun getSampleCardList(): List<GameCard> {
+    return listOf(
+        GameCard(
+            name = "Village",
+            expansion = GameCard.Expansion.BASE,
+            types = listOf(GameCard.Type.ACTION),
+            effects = listOf(GameCard.Effect.CARD, GameCard.Effect.ACTION),
+            cost = 3,
+            imageResId = R.drawable.ic_launcher_foreground,
+            onClick = {}
+        ),
+        GameCard(
+            name = "Smithy",
+            expansion = GameCard.Expansion.BASE,
+            types = listOf(GameCard.Type.ACTION),
+            effects = listOf(GameCard.Effect.CARD),
+            cost = 4,
+            imageResId = R.drawable.ic_launcher_foreground,
+            onClick = {}
+        ),
+        GameCard(
+            name = "Market",
+            expansion = GameCard.Expansion.BASE,
+            types = listOf(GameCard.Type.ACTION),
+            effects = listOf(
+                GameCard.Effect.CARD,
+                GameCard.Effect.ACTION,
+                GameCard.Effect.BUY,
+                GameCard.Effect.GOLD
+            ),
+            cost = 5,
+            imageResId = R.drawable.ic_launcher_foreground,
+            onClick = {}
+        ),
+        GameCard(
+            name = "Laboratory",
+            expansion = GameCard.Expansion.BASE,
+            types = listOf(GameCard.Type.ACTION),
+            effects = listOf(GameCard.Effect.CARD, GameCard.Effect.ACTION),
+            cost = 5,
+            imageResId = R.drawable.ic_launcher_foreground,
+            onClick = {}
+        )
+    )
+}
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             DominionHelperTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                DominionHelperTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        CardList(getSampleCardList())
+                    }
                 }
             }
         }
     }
 }
 
+// CardView displays a single card, with an image and a name
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CardView(card: GameCard) {
+    Card(modifier = Modifier.padding(8.dp)) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = card.imageResId),
+                contentDescription = "${card.name} Image",
+                modifier = Modifier.size(48.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text = card.name)
+            }
+        }
+    }
 }
-
-@Preview(showBackground = true)
+// CardList is a composable function that creates a list of cards
 @Composable
-fun GreetingPreview() {
-    DominionHelperTheme {
-        Greeting("Android")
+fun CardList(cardList: List<GameCard>) {
+    LazyColumn {
+        items(cardList) { card ->
+            CardView(card)
+        }
     }
 }
